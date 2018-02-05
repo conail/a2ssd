5aab3e43420fcd868e2b60c98adda5a3f767605f
Conail Stewart <conail@users.noreply.github.com>
2018-02-05 16:00:02 -0700

    Consistent formatting.

--- a2ssdqub/a2ssdqub/AddNewCustomer.cs
+++ a2ssdqub/a2ssdqub/AddNewCustomer.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq; // used for radio button, a fluent API
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq;
 using System.Windows.Forms;
-using a2ssdqub.DAL; // >> ADD THIS IN TO 028Educate
-using a2ssdqub.Models; // CustomerDAL.AddNew will now work
+using a2ssdqub.DAL; 
+using a2ssdqub.Models;
 
 namespace a2ssdqub
 {
@@ -19,42 +14,26 @@ namespace a2ssdqub
         public AddNewCustomer()
         {
             InitializeComponent();
-            lblIdMsg.Text = ID_ALERT;
-            tssText.Text = "";
+            ClearForm();
         }
 
         private void btnAddCustomer_Click(object sender, EventArgs e)
         {
-            // Quickest, most efficient way to do it; uses Linq
-            // FirstOrDef takes IEnumerable and returns an instance of IEnumerable???
-            // Tries to keep the IEnumerable alive as long as possible to chain it to other functions
-            // Iterates through and checks which are of type RadioButton
-            // Checks where the Checked property is true
-            // FirstOrDefault - pins it to one value, a reducing function
-            // Microsoft are v proud of Linq and have written it for almost any data set out there
-            // They started a project Linq To SQL, so you could just know Linq not SQL
             var checkedButton = this.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
-
-            if (txtForename.Text != "" && datDob.Text != "" && checkedButton.Text != "")
-            {
-                int newlyMadeID = CustomerDAL.Add(new Customer(txtForename.Text, datDob.Value, checkedButton.Text));
-
-                if(newlyMadeID > -1)
-                {
-                    string newsflash = txtForename.Text + " has been successfully added, with Customer ID " + newlyMadeID;
-                    MessageBox.Show(newsflash);
-                    // lblIdMsg.Text = "Customer ID " + newlyMadeID;
-                    tssText.Text = newsflash;
-                    ClearForm();
-                }
-                else
-                {
-                    MessageBox.Show("ERROR: NO NEW CUSTOMER ADDED.  Please try again.");
-                }
-            }
-            else
+            if (txtForename.Text == "" || datDob.Text == "" || checkedButton.Text == "")
             {
                 MessageBox.Show("Please complete all fields");
+                return;
+            }
+            
+            int id = CustomerDAL.Add(new Customer(txtForename.Text, datDob.Value, checkedButton.Text));
+            if(id > 0)
+            {
+                var str = string.Format("{0} has been successfully added, with Customer ID {1}.", txtForename.Text, id);
+                MessageBox.Show(str);
+                tssText.Text = str;
+                ClearForm();
+                MessageBox.Show("ERROR: NO NEW CUSTOMER ADDED.  Please try again.");
             }
         }
 
@@ -67,15 +46,8 @@ namespace a2ssdqub
         private void ClearForm()
         {
             lblIdMsg.Text = ID_ALERT;
-            txtForename.Text = "";
-            datDob.Text = "";
-
-            radMale.Checked = false;
-            radFemale.Checked = false;
-            radUndisclosed.Checked = false;
-            radUnknown.Checked = false;
-
-            // tssText.Text = "";
+            txtForename.Text = datDob.Text = "";
+            this.Controls.OfType<RadioButton>().Select(x => x.Checked = false);
         }
 
         private void button1_Click(object sender, EventArgs e)
